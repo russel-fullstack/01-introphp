8eb1fd9d533a321716102e5528df48528548b2e4
russel-fullstack <russeldev95@gmail.com>
2026-03-25 16:23:14 +0100

    comments

--- index.php
+++ index.php
@@ -7,5 +7,22 @@
     <h1><?php echo "Bienvenue sur ma page PHP !"; ?></h1>
     <p>Nous sommes en l'an : <?php echo date("Y-m-d"); ?></p>
     <p>Heure actuelle : <?= date("H:i:s") ?></p>
+
 </body>
 </html>
+<?php
+// Commentaire sur une seule ligne
+
+# Autre commentaire sur une seule ligne (style shell)
+
+/*
+  Commentaire sur plusieurs lignes
+  pour des explications plus longues
+*/
+
+/**
+ * Commentaire DocBlock
+ * Utilisé pour la documentation technique
+ * @param string $nom Le nom de l'utilisateur
+ */
+?>
