7c16f6edad214f491a9639caca76283ebc777b86
russel-fullstack <russeldev95@gmail.com>
2026-03-25 17:16:33 +0100

    assignation

--- 02-variable.php
+++ 02-variable.php
@@ -1,11 +1,21 @@
 <?php
 
 echo '<h1>Voici du variables PHP dans une page HTML</h1>
-<a href="index.php">Retour à l\'accueil</a> !';
-$nom = "Jean";       // Chaîne de caractères (String)
+<a href="index.php">Retour à l\'accueil</a> ';
+echo $nom = "Jean"; ;      // Chaîne de caractères (String)
 
 $age = 25;           // Entier (Integer)
 
 $prix = 19.99;       // Nombre à virgule (Float)
 
 $estActif = true;    // Booléen (Boolean)
+
+$message = "Bonjour";        // Assigne une valeur
+$message = "Au revoir";      // Réassigne (écrase la valeur précédente)
+
+$a = $b = $c = 10; 
+
+$nomVar = "salutation";
+$$nomVar = "Bonjour !";  // Crée la variable $salutation
+
+echo $salutation;  // Affiche : Bonjour !          // Assignation multiple
