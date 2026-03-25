c126965e20c5fc376c7d6f8c7e5c8c877f364bfc
russel-fullstack <russeldev95@gmail.com>
2026-03-25 16:55:00 +0100

    variable

--- 02-variable.php
+++ 02-variable.php
@@ -2,3 +2,10 @@
 
 echo '<h1>Voici du variables PHP dans une page HTML</h1>
 <a href="index.php">Retour à l\'accueil</a> !';
+$nom = "Jean";       // Chaîne de caractères (String)
+
+$age = 25;           // Entier (Integer)
+
+$prix = 19.99;       // Nombre à virgule (Float)
+
+$estActif = true;    // Booléen (Boolean)
