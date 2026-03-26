7588d71799bbe85261112628c66af4a7ab383040
russel-fullstack <russeldev95@gmail.com>
2026-03-26 10:44:19 +0100

    test

--- 04-type-declaration.php
+++ 04-type-declaration.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 echo '<h1>Voici du variables PHP dans une page HTML</h1>
 <a href="index.php">Retour à l\'accueil</a> ';
+echo "hello";
 
 
 function saluer(string $nom): void {
